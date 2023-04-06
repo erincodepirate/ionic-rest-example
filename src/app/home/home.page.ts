9933d7e9c5c4b80de6843abf7d810f8c0ee7d6af
Erin Clark <erincodepirate@gmail.com>
2023-04-05 17:46:30 -0700

    Fetch all data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +8,26 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  data: any[] = [];
+
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.http.get<any>('https://jsonplaceholder.typicode.com/todos').subscribe({
+    next:data => {
+      console.log(data);
+      this.data = data;
+    }, 
+    error:e=> {
+      console.log(e);
+    }})
+  }
 }
